cc50ffa56b1d51d6bcf88954a3e8c83bd23bfbe0
Gitfirul Aziz Widiyanto <64318003+gitfirul01@users.noreply.github.com>
2023-04-25 09:07:15 +0700

    add relative axes coordinate

--- python/test1.py
+++ python/test1.py
@@ -106,6 +106,34 @@ while True:
     ax.plot([T02[0,3], x],          [T02[1,3], y],          [T02[2,3], z],          'b')
     ax.scatter(x, y, z)
 
+
+    # define relative coordinate frames for each link
+    O = np.array([[0, 0, 0]]).T
+    X1 = T01[:3, :3] @ np.array([[1, 0, 0]]).T
+    Y1 = T01[:3, :3] @ np.array([[0, 1, 0]]).T
+    Z1 = T01[:3, :3] @ np.array([[0, 0, 1]]).T
+
+    X2 = T02[:3, :3] @ np.array([[1, 0, 0]]).T
+    Y2 = T02[:3, :3] @ np.array([[0, 1, 0]]).T
+    Z2 = T02[:3, :3] @ np.array([[0, 0, 1]]).T
+
+    X3 = T03[:3, :3] @ np.array([[1, 0, 0]]).T
+    Y3 = T03[:3, :3] @ np.array([[0, 1, 0]]).T
+    Z3 = T03[:3, :3] @ np.array([[0, 0, 1]]).T
+
+    ax.quiver(T01[0, 3], T01[1, 3], T01[2, 3], X1[0, 0], X1[1, 0], X1[2, 0], color='r', length=25)
+    ax.quiver(T01[0, 3], T01[1, 3], T01[2, 3], Y1[0, 0], Y1[1, 0], Y1[2, 0], color='g', length=25)
+    ax.quiver(T01[0, 3], T01[1, 3], T01[2, 3], Z1[0, 0], Z1[1, 0], Z1[2, 0], color='b', length=25) 
+    
+    ax.quiver(T02[0, 3], T02[1, 3], T02[2, 3], X2[0, 0], X2[1, 0], X2[2, 0], color='r', length=25)
+    ax.quiver(T02[0, 3], T02[1, 3], T02[2, 3], Y2[0, 0], Y2[1, 0], Y2[2, 0], color='g', length=25)
+    ax.quiver(T02[0, 3], T02[1, 3], T02[2, 3], Z2[0, 0], Z2[1, 0], Z2[2, 0], color='b', length=25) 
+    
+    ax.quiver(T03[0, 3], T03[1, 3], T03[2, 3], X3[0, 0], X3[1, 0], X3[2, 0], color='r', length=25)
+    ax.quiver(T03[0, 3], T03[1, 3], T03[2, 3], Y3[0, 0], Y3[1, 0], Y3[2, 0], color='g', length=25)
+    ax.quiver(T03[0, 3], T03[1, 3], T03[2, 3], Z3[0, 0], Z3[1, 0], Z3[2, 0], color='b', length=25) 
+    
+
     ## set figure configuration
     # ax.set_title('3D Simulation')
     ax.set_xlabel('X')
